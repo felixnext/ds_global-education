b239f7031a3107fa646a5bb1a07e5b774508439e
Felix Geilert <root92@live.de>
2019-08-31 11:32:47 +0200

    feat: Implemented all extraction functions for education

---
+++ economic_utils.py
@@ -0,0 +1,13 @@
+'''
+Helper functions to handle economic indicators.
+
+author: Felix Geilert
+'''
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import os
+from matplotlib import pyplot as plt
+
+# TODO:
--- edstats_utils.py
+++ edstats_utils.py
@@ -105,9 +105,9 @@ def _retrieve_indicator(df, reg_str):
 def retrieve_wittgenstein_mean_schooling(df):
     '''Retrieve the Wittgenstein Projections for mean years of education across different age groups.
     
-    TODO: Explain Wittgenstein
-    
-    Function should extract all relevant wittgenstein indicators
+    Mean number of years spent in school by age group and gender. 
+    Projections are based on collected census and survey data for the base year (around 2010) and the Medium Shared Socioeconomic Pathways (SSP2) projection model. 
+    The SSP2 is a middle-of-the-road scenario that combines medium fertility with medium mortality, medium migration, and the Global Education Trend (GET) education scenario.
     
     Args:
         df (pd.DataFrame): df_data Dataframe
@@ -128,6 +128,10 @@ def retrieve_wittgenstein_mean_schooling(df):
 def retrieve_wittgenstein_population(df):
     '''Retrieve the Wittgenstein Projections for Education of the Population.
     
+    Share of the population of the stated age group that has completed upper secondary or incomplete post-secondary education as the highest level of educational attainment. 
+    Projections are based on collected census and survey data for the base year (around 2010) and the Medium Shared Socioeconomic Pathways (SSP2) projection model. 
+    The SSP2 is a middle-of-the-road scenario that combines medium fertility with medium mortality, medium migration, and the Global Education Trend (GET) education scenario. 
+    
     Args:
         df (pd.DataFrame): df_data DataFrame
         
@@ -144,15 +148,16 @@ def retrieve_wittgenstein_population(df):
     
     return df_witt
 
-def retrieve_barrolee_population():
+def retrieve_barrolee_percentage():
     '''Retrieve the Barro-Lee Indicators for education of different parts of the population in percentage.
     
-    TODO: explain Barro-Lee
-    
-    Function should extract all relevant barro lee indicators
+    Percentage of population of the stated group that has completed or incompleted education of the stated type.
     
     Args:
         df (pd.DataFrame): df_data DataFrame
+        
+    Returns:
+        Dataframe with all datapoints for the indicator and additional columns: `['indicator', 'schooling', 'completed', 'age_group', 'gender']`
     '''
     # extract relevant data
     df_barro = _retrieve_indicator(df, 'Barro-Lee: Percentage of .*')
@@ -166,6 +171,9 @@ def retrieve_barrolee_population():
     
 def retrieve_barrolee_years(df):
     '''Retrieve the Barro-Lee indicators for average years in different types of education.
+    
+    Average years of stated schooling in the given age group.
+    total is the average years of stated education completed among people in the given group.
     
     Args:
         df (pd.DataFrame): df_data DataFrame
@@ -182,3 +190,170 @@ def retrieve_barrolee_years(df):
     df_barro = pd.concat([df_barro, df_barro_groups[['indicator', 'schooling', 'age_group', 'gender']]], axis=1)
     
     return df_barro
+
+def retrieve_education_length(df):
+    '''Retrieves the theoretical length of different types of education.
+    
+    Theoretical duration of stated education in years.
+    
+    Args:
+        df (pd.DataFrame): df_data DataFrame
+    
+    Returns:
+        Dataframe with all datapoints for the indicator and additional columns: `['education']`
+    '''
+    # retrieve data
+    df_edu = _retrieve_indicator(df, 'Theoretical duration of .* education (years)')
+    
+    # split data
+    df_edu_groups = df_edu['Indicator Name'].str.extract('Theoretical duration of (?P<education>[\s\S]+) education (years)', expand=True)
+    df_edu = pd.concat([df_edu, df_edu_groups[['education']]], axis=1)
+    
+    return df_edu
+
+def retrieve_gdp_education_institution(df):
+    '''Retrieve Indicators related to GDP expenditure in education institutions.
+    
+    Government expenditure in stated institution as % of GDP (%)  
+    
+    Args:
+        df (pd.DataFrame): df_data DataFrame
+    
+    Returns:
+        Dataframe with all datapoints for the indicator and additional columns: `[institution]`
+    '''
+    # retrieve data
+    df_gdp = _retrieve_indicator(df, 'Government expenditure in .* as % of GDP (%)')
+    
+    # split data
+    df_gdp_groups = df_gdp['Indicator Name'].str.extract('Government expenditure in (?P<institution>[\s\S]+) as % of GDP (%)', expand=True)
+    df_gdp = pd.concat([df_gdp, df_gdp_groups[['institution']]], axis=1)
+    
+    return df_gdp
+
+def retrieve_gdp_education(df):
+    '''Retrieve Indicators related to GDP expenditure for education.
+    
+    Government expenditure in stated education form as % of GDP (%)  
+    
+    Args:
+        df (pd.DataFrame): df_data DataFrame
+    
+    Returns:
+        Dataframe with all datapoints for the indicator and additional columns: `[education]`
+    '''
+    # retrieve data
+    df_gdp = _retrieve_indicator(df, 'Government expenditure on .* education as % of GDP (%)')
+    
+    # split data
+    df_gdp_groups = df_gdp['Indicator Name'].str.extract('Government expenditure on (?P<education>[\s\S]+) education as % of GDP (%)', expand=True)
+    df_gdp = pd.concat([df_gdp, df_gdp_groups[['education']]], axis=1)
+    
+    return df_gdp
+
+def retrieve_skill_llece(df):
+    '''Retrieve the proficiency on various skills on LLECE levels in % of entire population.
+    
+    Percentage of stated grade students in the given proficiency level on the LLECE scale for the subject. 
+    Data reflects country performance in the stated year according to LLECE reports.
+    
+    Args:
+        df (pd.DataFrame): df_data DataFrame
+    
+    Returns:
+        DataFrame with all datapoints for the indicators and additional columns `[indicator, gender, grade, subject, level]`
+    '''
+    # retrieve relevant data
+    df_skill = _retrieve_indicator(df, 'LLECE: .* students by .* proficiency level (%)')
+    
+    # extract columns
+    df_skill_groups = df_skill['Indicator Name'].str.extract('LLECE: (?P<indicator>(?P<gender>(Male|Female))?[ ]*(?P<grade>[0-9]+(rd|th) grade) students by (?P<subject>.*?) proficiency level \(%\)\. (?P<level>[\S ]+))', expand=True)
+    df_skill = pd.concat([df_skill, df_skill_groups[['indicator', 'gender', 'grade', 'subject', 'level']].fillna({'gender': 'Total'})], axis=1)
+    
+    return df_skill
+
+def retrieve_skill_pisa(df):
+    '''Retrieve 
+    
+    Percentage of 15-year-old students at the given proficiency level on the PISA subject scale. 
+    Data reflects country performance in the stated year according to PISA reports, but may not be comparable across years or countries.
+    
+    Args:
+        df (pd.DataFrame): df_data Dataframe
+    
+    Returns:
+        DataFrame with all datapoints for the indicators and additional columns `[indicator, gender, subject, level]`
+    '''
+    # retrieve relevant data
+    df_skill = _retrieve_indicator(df, 'PISA: .* proficiency level')
+    
+    # extract columns
+    df_skill_groups = df_skill['Indicator Name'].str.extract('PISA: (?P<indicator>(?P<gender>(Male|Female))?[ ]*[0-9]+-year-olds by (?P<subject>.*?) proficiency level \(%\)\. (?P<level>[\S ]+))', expand=True)
+    df_skill = pd.concat([df_skill, df_skill_groups[['indicator', 'gender', 'subject', 'level']].fillna({'gender': 'Total'})], axis=1)
+    
+    return df_skill
+
+def retrieve_skill_timss(df):
+    '''Retrieve 
+    
+    Stated grade students reaching the given international benchmark of achievement in the given subject in (%). 
+    They are the share of given grade students scoring in the given achievement bracket on the subject assessment. 
+    Data reflects country performance in the stated year according to TIMSS reports, but may not be comparable across years or countries.
+    
+    Args:
+        df (pd.DataFrame): df_data Dataframe
+    
+    Returns:
+        DataFrame with all datapoints for the indicators and additional columns `[indicator, gender, subject, achievement]`
+    '''
+    # retrieve relevant data
+    df_skill = _retrieve_indicator(df, 'TIMSS: .* achievement')
+    
+    # extract columns
+    df_skill_groups = df_skill['Indicator Name'].str.extract('TIMSS: (?P<indicator>(?P<gender>(Male|Female))?[ ]*(?P<grade>.*) grade students (?P<achievement>(reaching the .*?|who did not reach the low) international benchmark) of (?P<subject>.*) achievement \(%\))', expand=True)
+    df_skill = pd.concat([df_skill, df_skill_groups[['indicator', 'gender', 'subject', 'achievement']].fillna({'gender': 'Total'})], axis=1)
+    
+    return df_skill
+
+def retrieve_skill_piaac(df):
+    '''Retrieve the relevant PIAAC Data for
+    
+    The Programme for the International Assessment of Adult Competencies (PIAAC) is a programme of assessment and analysis of adult skills. 
+    The major survey conducted as part of PIAAC is the Survey of Adult Skills. 
+    The Survey measures adults proficiency in key information-processing skills - literacy, numeracy and problem solving - and gathers information and data on how adults use their skills at home, at work and in the wider community.
+    
+    PIAAC: Adults by literacy proficiency level (%). Below Level 1
+    PIAAC: Adults by proficiency level in problem solving in technology-rich environments (%). Below Level 1
+    PIAAC: Female adults by numeracy proficiency level (%). Below Level 1
+    
+    Args:
+        df (pd.DataFrame): df_data Dataframe
+    
+    Returns:
+        DataFrame with all datapoints for the indicators and additional columns `[indicator, gender, subject, achievement]`
+    '''
+    # retrieve relevant data
+    df_skill = _retrieve_indicator(df, 'PIAAC: .* proficiency level')
+    
+    # extract columns
+    df_skill_groups = df_skill['Indicator Name'].str.extract('PIAAC: (?P<indicator>(?P<gender>(Male|Female))?[ ]*(?P<type>Young)?[ ]*[aA]dults by .*?(?P<subject>((?<=proficiency level in ).*|.*(?= proficiency level))).*? \(%\). (?P<level>[\S ]+))', expand=True)
+    df_skill = pd.concat([df_skill, df_skill_groups[['indicator', 'gender', 'type', 'subject', 'level']].fillna({'gender': 'Total', 'type': 'all'})], axis=1)
+    
+    return df_skill
+
+def filter_years(df, years):
+    '''Takes a dataframe and filters only the year columns (leaves all other columns intact).
+    
+    Args:
+        df (pd.DataFrame): Dataframe to be filtered
+        years (list): list of year strings to be filtered for
+        
+    Returns:
+        Dataframe with all relevant columns but only the data columns for the given year
+    '''
+    # generate a list of data
+    years = list(range(1970, 2018)) + list(range(2020, 2101, 5))
+    rm = ['2010', '2015']
+    years = [str(y) for y in years if str(y) not in rm]
+    # drop the irrelevant years
+    return df.drop(years, axis=1)
