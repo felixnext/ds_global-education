fbb472e5844eb4ebff2731086a36483dc045fcd6
Felix Geilert <root92@live.de>
2019-09-01 20:49:18 +0200

    feat: implemented correlation analysis for second question
    * General data analysis implemented
    * Fixed some bugs
    * Missing: Modularization of correlation and execution over all relevant features

--- datasets/indicators/get_indicators.sh
+++ datasets/indicators/get_indicators.sh
@@ -6,5 +6,7 @@ wget -O child-mortality.zip http://api.worldbank.org/v2/en/indicator/SH.DYN.MORT
 wget -O income-lowest-20.zip http://api.worldbank.org/v2/en/indicator/SI.DST.FRST.20?downloadformat=csv
 wget -O access-electricity.zip http://api.worldbank.org/v2/en/indicator/EG.ELC.ACCS.ZS?downloadformat=csv
 wget -O pop-growth.zip http://api.worldbank.org/v2/en/indicator/SP.POP.GROW?downloadformat=csv
-
+wget -O gdp-capita-ppp.zip http://api.worldbank.org/v2/en/indicator/NY.GDP.PCAP.PP.CD?downloadformat=csv
+wget -O gdp.zip http://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=csv
+wget -O edu-of-gdp.zip http://api.worldbank.org/v2/en/indicator/SE.XPD.TOTL.GD.ZS?downloadformat=csv
 unzip '*.zip'
--- economic_utils.py
+++ economic_utils.py
@@ -11,3 +11,26 @@ import os
 from matplotlib import pyplot as plt
 
 # TODO:
+def _get_csv(file):
+  datapath = "./datasets/indicators"
+  return pd.read_csv(os.path.join(datapath, file), skiprows=4)
+
+def retrieve_gdp():
+  '''Retrieves the GDP values of the countries.'''
+  return _get_csv("API_NY.GDP.MKTP.CD_DS2_en_csv_v2_126271.csv")
+
+def retrieve_gdp_pc():
+  ''''Retrieves GDP per Capita'''
+  return _get_csv("API_NY.GDP.PCAP.PP.CD_DS2_en_csv_v2_126302.csv")
+
+def retrieve_electricity():
+  '''Retrieve Data of percentage of population with acccess to electricity.'''
+  return _get_csv("API_EG.ELC.ACCS.ZS_DS2_en_csv_v2_126505.csv")
+
+def retrieve_education_spending():
+  '''Retreive percentage of GDP spend on education.'''
+  return _get_csv("API_SE.XPD.TOTL.GD.ZS_DS2_en_csv_v2_126172.csv")
+
+def retrieve_income():
+  '''Retrieve percentage of income going to poorest 20 percent.'''
+  return _get_csv("API_SI.DST.FRST.20_DS2_en_csv_v2_42586.csv")
